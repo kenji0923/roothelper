b31cdf853f209fc0b3ec60738543c6daa0436931
SHU Kenji <kenji0923@gmail.com>
2026-06-01 18:29:24 -0400

    Bug include order in Interpreter mode fixed

--- ROOT_helper.h
+++ ROOT_helper.h
@@ -5,13 +5,13 @@
 
 #define ROOT_HELPER_USED_IN_INTERPRETER
 
-#include "src/core/analysis.cpp"
-#include "src/core/container.cpp"
-#include "src/core/data_saver.cpp"
-#include "src/core/graphics.cpp"
 #include "src/core/include/ROOT_helper/analysis.h"
+#include "src/core/include/ROOT_helper/graphics.h"
 #include "src/core/include/ROOT_helper/container.h"
 #include "src/core/include/ROOT_helper/data_saver.h"
-#include "src/core/include/ROOT_helper/graphics.h"
+#include "src/core/analysis.cpp"
+#include "src/core/graphics.cpp"
+#include "src/core/container.cpp"
+#include "src/core/data_saver.cpp"
 
 #endif  // ROOT_HELPER_INTERPRETER_H_
