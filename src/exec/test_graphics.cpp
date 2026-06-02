c12c14075fa699fde842d3417bb65f811859e308
SHU Kenji <kenji0923@gmail.com>
2026-06-01 20:24:36 -0400

    Merge 2D graphic test to test_graphics.cpp and output to test_macro/GraphicsTest

--- src/exec/test_graphics.cpp
+++ src/exec/test_graphics.cpp
@@ -1,19 +1,23 @@
 #include <roothelper/roothelper.h>
 
 #include <string>
+#include <cmath>
 
 #include <TCanvas.h>
 #include <TF1.h>
 #include <TMath.h>
+#include <TGraph2D.h>
+#include <TRandom.h>
 
 
 namespace rh = roothelper;
 
 
 TCanvas* create_test_canvas(std::string canvas_name);
+TCanvas* create_test_canvas_2d(std::string canvas_name);
 
 
-rh::DataSaver data_saver("GraphicsTest");
+rh::DataSaver data_saver("test_macro/GraphicsTest");
 
 
 int main(int argc, char** argv)
@@ -27,7 +31,6 @@ int main(int argc, char** argv)
      */
     create_test_canvas("SinglePad_8pt_LabelAtXend");
 
-
     /**
      * Single pad.
      * 10pt text size with 86mm width.
@@ -35,6 +38,14 @@ int main(int argc, char** argv)
      */
     rh::GraphicsSize::current = rh::g_size_10pt;
     create_test_canvas("SinglePad_10pt_LabelAtXend");
+
+
+    /**
+     * 2D graph with COLZ.
+     * 8pt text size.
+     */
+    rh::GraphicsSize::current = rh::g_size_8pt;
+    create_test_canvas_2d("Graph2DTest");
 
     return 0;
 }
@@ -53,6 +64,33 @@ TCanvas* create_test_canvas(std::string canvas_name)
     f_wave->GetXaxis()->SetTitle("xyzABC (arb. units)");
     f_wave->GetYaxis()->SetTitle("yzxBCA (arb. units)");
     std::cout << rh::IncreaseRightMargin(1) << std::endl;
+
+    data_saver.WriteCanvas(c);
+
+    return c;
+}
+
+
+TCanvas* create_test_canvas_2d(std::string canvas_name)
+{
+    canvas_name = std::string("c_") + canvas_name;
+    TCanvas* c = rh::CreateCanvas(canvas_name.c_str(), "TGraph2D Test with COLZ");
+
+    TGraph2D* g = new TGraph2D();
+    g->SetName("g_2d");
+    g->SetTitle("TGraph2D COLZ Test;X Title;Y Title;Z Title (Color Palette)");
+
+    TRandom r;
+    int n = 200;
+    for (int i = 0; i < n; ++i) {
+        double x = r.Uniform(-2, 2);
+        double y = r.Uniform(-2, 2);
+        double z = x*x * std::exp(-x*x - y*y);
+        g->SetPoint(i, x, y, z);
+    }
+
+    g->Draw("COLZ");
+    rh::SetAxes(g, "COLZ");
 
     data_saver.WriteCanvas(c);
 
