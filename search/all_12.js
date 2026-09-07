4a222a47357d8b77d7de3b562e963a4c2938eac8
kenji0923 <kenji0923@gmail.com>
2026-09-07 16:43:14 +0000

    Deploying to gh-pages from @ kenji0923/roothelper@a7ebf6860e22324c2b3621612197545698bd9a61 🚀

--- search/all_12.js
+++ search/all_12.js
@@ -7,13 +7,14 @@ var searchData=
   ['server_20protocol_20setup_4',['&lt;tt&gt;CreateRootClangd&lt;/tt&gt; (Language Server Protocol Setup)',['../index.html#autotoc_md21',1,'']]],
   ['setup_5',['&lt;tt&gt;CreateRootClangd&lt;/tt&gt; (Language Server Protocol Setup)',['../index.html#autotoc_md21',1,'']]],
   ['setxaxis_6',['SetXAxis',['../namespaceroothelper.html#a97857b1eb25d67fdfd7bcba596c72972',1,'roothelper']]],
-  ['solvers_7',['Styling &amp; Axis Solvers',['../index.html#autotoc_md17',1,'']]],
-  ['storage_20structure_8',['Subdirectory Storage Structure',['../index.html#autotoc_md14',1,'']]],
-  ['structure_9',['Subdirectory Storage Structure',['../index.html#autotoc_md14',1,'']]],
-  ['styling_20axis_20solvers_10',['Styling &amp; Axis Solvers',['../index.html#autotoc_md17',1,'']]],
-  ['subdirectory_20storage_20structure_11',['Subdirectory Storage Structure',['../index.html#autotoc_md14',1,'']]],
-  ['submodule_20tt_20analysis_20h_20tt_12',['1. Analysis Submodule (&lt;tt&gt;analysis.h&lt;/tt&gt;)',['../index.html#autotoc_md2',1,'']]],
-  ['submodule_20tt_20container_20h_20tt_13',['2. Container Submodule (&lt;tt&gt;container.h&lt;/tt&gt;)',['../index.html#autotoc_md5',1,'']]],
-  ['submodule_20tt_20data_5fsaver_20h_20tt_14',['3. DataSaver Submodule (&lt;tt&gt;data_saver.h&lt;/tt&gt;)',['../index.html#autotoc_md12',1,'']]],
-  ['submodule_20tt_20graphics_20h_20tt_15',['4. Graphics Submodule (&lt;tt&gt;graphics.h&lt;/tt&gt;)',['../index.html#autotoc_md16',1,'']]]
+  ['setyaxis_7',['SetYAxis',['../namespaceroothelper.html#a3e325e3d5cd80a298ed7845fea292f5e',1,'roothelper']]],
+  ['solvers_8',['Styling &amp; Axis Solvers',['../index.html#autotoc_md17',1,'']]],
+  ['storage_20structure_9',['Subdirectory Storage Structure',['../index.html#autotoc_md14',1,'']]],
+  ['structure_10',['Subdirectory Storage Structure',['../index.html#autotoc_md14',1,'']]],
+  ['styling_20axis_20solvers_11',['Styling &amp; Axis Solvers',['../index.html#autotoc_md17',1,'']]],
+  ['subdirectory_20storage_20structure_12',['Subdirectory Storage Structure',['../index.html#autotoc_md14',1,'']]],
+  ['submodule_20tt_20analysis_20h_20tt_13',['1. Analysis Submodule (&lt;tt&gt;analysis.h&lt;/tt&gt;)',['../index.html#autotoc_md2',1,'']]],
+  ['submodule_20tt_20container_20h_20tt_14',['2. Container Submodule (&lt;tt&gt;container.h&lt;/tt&gt;)',['../index.html#autotoc_md5',1,'']]],
+  ['submodule_20tt_20data_5fsaver_20h_20tt_15',['3. DataSaver Submodule (&lt;tt&gt;data_saver.h&lt;/tt&gt;)',['../index.html#autotoc_md12',1,'']]],
+  ['submodule_20tt_20graphics_20h_20tt_16',['4. Graphics Submodule (&lt;tt&gt;graphics.h&lt;/tt&gt;)',['../index.html#autotoc_md16',1,'']]]
 ];
